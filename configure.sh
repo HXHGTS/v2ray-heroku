f6a53daa537586026c81a24846b38163a2180098
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-11-09 02:09:24 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -22,7 +22,7 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "clients": [
                     {
                         "id": "$UUID",
-                        "alterId": 2
+                        "alterId": 0
                     }
                 ],
                 "disableInsecureEncryption": true
